21c7efb638652b9133f3b89e39a62d1a5482a980
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2024-11-21 17:30:29 +0000

    made = button work

--- app.js
+++ app.js
@@ -6,28 +6,36 @@ const app = new Vue({
         operator: '+',
         total: 0
     },
-    computed: {
-        calcTotal: function() {
-            if (this.operator == '+') {
-                return this.num1 + this.num2;
-            } else if (this.operator == '-') {
-                return this.num1 - this.num2;
-            } else if (this.operator == '*') {
-                return this.num1 * this.num2;
-            } else if (this.operator == '/') {
-                return this.num1 / this.num2;
-            } else {
-                return 'No operator chosen, try again!';
-            }
-            
-        }
-    },
     methods: {
         resetFields: function() {
             this.num1 = 0,
             this.num2 = 0,
             this.operator = '+',
             this.total = 0
+        },
+        setOperator: function(op) {
+            this.operator = op;
+        },
+        calcTotal: function() {
+            let newValue;
+            if (this.operator == '+') {
+                newValue = this.num1 + this.num2;
+            } else if (this.operator == '-') {
+                newValue = this.num1 - this.num2;
+            } else if (this.operator == '*') {
+                newValue = this.num1 * this.num2;
+            } else if (this.operator == '/' && this.num2 === 0) {
+                newValue = 'Please don\'t divide by zero!';
+            } else if (this.operator == '/') {
+                newValue = this.num1 / this.num2;
+            } else {
+                newValue = 'I do not know how you broke this, but you did.';
+            };
+            console.log("num1 = ", this.num1, "num2 = ", this.num2, "operator = ", this.operator, "total = ", this.total);
+            this.$set(this, 'total', newValue);
         }
+
+    },
+    computed: {
     }
 });
