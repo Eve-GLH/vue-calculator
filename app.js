72645dbd5191a65e580d3235d8a7e9900c0acfd5
EveBookingOnline <eve@bookingonline.co.uk>
2024-11-21 21:06:52 +0000

    replaced inputs with number buttons

--- app.js
+++ app.js
@@ -3,38 +3,54 @@ const app = new Vue({
     data: {
         num1: 0,
         num2: 0,
-        operator: '+',
+        operator: '',
         total: 0
     },
     methods: {
         resetFields: function() {
             this.num1 = 0,
             this.num2 = 0,
-            this.operator = '+',
+            this.operator = '',
             this.total = 0
         },
         setOperator: function(op) {
             this.operator = op;
         },
+        
+        addDigit: function(di) {
+            if (this.operator === '') {
+                if (this.num1 == 0) {
+                    this.num1 = di;
+                } else {
+                    this.num1 = this.num1.toString() + di.toString();
+                }
+            } else {
+                if (this.num2 == 0) {
+                    this.num2 = di;
+                } else {
+                    this.num2 = this.num2.toString() + di.toString();
+                }
+            }
+        },
         calcTotal: function() {
             let newValue;
             if (this.operator == '+') {
-                newValue = this.num1 + this.num2;
+                newValue = Number(this.num1) + Number(this.num2);
             } else if (this.operator == '-') {
-                newValue = this.num1 - this.num2;
+                newValue = Number(this.num1) - Number(this.num2);
             } else if (this.operator == '*') {
-                newValue = this.num1 * this.num2;
+                newValue = Number(this.num1) * Number(this.num2);
             } else if (this.operator == '/' && this.num2 === 0) {
                 newValue = 'Please don\'t divide by zero!';
             } else if (this.operator == '/') {
-                newValue = this.num1 / this.num2;
+                newValue = Number(this.num1) / Number(this.num2);
             } else {
                 newValue = 'I do not know how you broke this, but you did.';
             };
-            console.log("num1 = ", this.num1, "num2 = ", this.num2, "operator = ", this.operator, "total = ", this.total);
+            //console.log("num1 = ", this.num1, "num2 = ", this.num2, "operator = ", this.operator, "total = ", this.total);
             this.$set(this, 'total', newValue);
+            this.$set(this, 'operator', '');
         }
-
     },
     computed: {
     }
